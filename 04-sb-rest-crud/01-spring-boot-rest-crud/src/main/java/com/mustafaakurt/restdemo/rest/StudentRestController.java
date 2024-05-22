60202e30c22b77295e946cf2382bdba6aa2c5e82
mustafaakurt <mustafaakurt@yandex.com>
2024-05-22 17:43:43 +0300

    PathVariable added

--- 04-sb-rest-crud/01-spring-boot-rest-crud/src/main/java/com/mustafaakurt/restdemo/rest/StudentRestController.java
+++ 04-sb-rest-crud/01-spring-boot-rest-crud/src/main/java/com/mustafaakurt/restdemo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.mustafaakurt.restdemo.rest;
 
 import com.mustafaakurt.restdemo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +14,28 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> studentList;
 
-        List<Student> studentList = new ArrayList<>();
+    // define @PostConstruct to load the data ... only once!
+    @PostConstruct
+    public void loadData(){
+
+        studentList = new ArrayList<>();
 
         studentList.add(new Student("Mustafa", "Kurt"));
         studentList.add(new Student("Mario", "Rossi"));
         studentList.add(new Student("Marry", "Smith"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return studentList;
     }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        return studentList.get(studentId);
+    }
+
 
 }
