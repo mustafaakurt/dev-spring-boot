8e2c5b986723148cca05632d8b52e3c3809686cf
mustafaakurt <mustafaakurt@yandex.com>
2024-05-22 19:04:40 +0300

    StudentRestExceptionHandler

---
+++ 04-sb-rest-crud/03-spring-boot-global-exception-handling/src/main/java/com/mustafaakurt/restdemo/rest/StudentRestController.java
@@ -0,0 +1,71 @@
+package com.mustafaakurt.restdemo.rest;
+
+import com.mustafaakurt.restdemo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> studentList;
+
+    // define @PostConstruct to load the data ... only once!
+    @PostConstruct
+    public void loadData() {
+
+        studentList = new ArrayList<>();
+
+        studentList.add(new Student("Mustafa", "Kurt"));
+        studentList.add(new Student("Mario", "Rossi"));
+        studentList.add(new Student("Marry", "Smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return studentList;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // check the studentId against list size
+        if (studentId >= studentList.size() || studentId < 0) {
+            throw new StudentNotFoundException("Student not found with id: " + studentId);
+        }
+
+        return studentList.get(studentId);
+    }
+    // add an exception handler using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exception){
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exception.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return a ResponseEntity
+        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
+
+    // add another exception handler ... to catch any exception (catch all)
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exception){
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exception.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return a ResponseEntity
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+    }
+
+
+}
---
+++ 04-sb-rest-crud/03-spring-boot-global-exception-handling/src/main/java/com/mustafaakurt/restdemo/rest/StudentRestExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.mustafaakurt.restdemo.rest;
+
+public class StudentRestExceptionHandler {
+}
